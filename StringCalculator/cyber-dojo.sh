067e342e0c38846e044a09ed05cc7a07dd864c7b
Steve Turley <steve@turleylabs.com>
2020-06-18 11:21:51 -0500

    Porting scripts to c#

--- StringCalculator/cyber-dojo.sh
+++ StringCalculator/cyber-dojo.sh
@@ -1,16 +1,20 @@
-#! /bin/bash
-set -e
+trap tidy_up EXIT
+function tidy_up()
+{
+  # cyber-dojo returns text files under /sandbox that are
+  # created/deleted/changed. In here you can remove any
+  # such files you don't want returned to the browser.
+  [ ! -f TestResult.xml ] || rm TestResult.xml
+}
 
-cd ${CYBER_DOJO_SANDBOX}
+NUNIT_PATH=/nunit/lib/net45
+export MONO_PATH=${NUNIT_PATH}
 
-CLASSES=.:`ls /junit/*.jar | tr '\n' ':'`
+mcs -t:library \
+  -r:${NUNIT_PATH}/nunit.framework.dll \
+  -out:RunTests.dll *.cs
 
-if javac --enable-preview --release 14 -Xlint:preview -Xlint:unchecked -Xlint:deprecation -cp $CLASSES *.java; then
-  java --enable-preview -jar /junit/junit-platform-console-standalone-1.6.2.jar \
-      --disable-banner \
-      --disable-ansi-colors \
-      --details=tree \
-      --details-theme=ascii \
-      --class-path . \
-      --scan-class-path
+if [ $? -eq 0 ]; then
+  NUNIT_RUNNERS_PATH=/nunit/tools
+  mono ${NUNIT_RUNNERS_PATH}/nunit3-console.exe --noheader ./RunTests.dll
 fi

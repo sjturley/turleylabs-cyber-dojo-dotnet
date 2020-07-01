b2e27cf4d0bdf44bc4c88536bbbd6fc0bed93efc
Steve Turley <steve@turleylabs.com>
2020-07-01 10:09:01 -0500

    Update cyber-dojo.sh

--- VideoStoreRefactoring/cyber-dojo.sh
+++ VideoStoreRefactoring/cyber-dojo.sh
@@ -1,6 +1,21 @@
-#! /bin/bash
 
-set -e
+trap tidy_up EXIT
+function tidy_up()
+{
+  # cyber-dojo returns text files under /sandbox that are
+  # created/deleted/changed. In here you can remove any
+  # such files you don't want returned to the browser.
+  [ ! -f TestResult.xml ] || rm TestResult.xml
+}
 
-dotnet restore -s /packages
-dotnet test ./VideoStore.csproj
+NUNIT_PATH=/nunit/lib/net45
+export MONO_PATH=${NUNIT_PATH}
+
+mcs -t:library \
+  -r:${NUNIT_PATH}/nunit.framework.dll \
+  -out:RunTests.dll *.cs
+
+if [ $? -eq 0 ]; then
+  NUNIT_RUNNERS_PATH=/nunit/tools
+  mono ${NUNIT_RUNNERS_PATH}/nunit3-console.exe --noheader ./RunTests.dll
+fi

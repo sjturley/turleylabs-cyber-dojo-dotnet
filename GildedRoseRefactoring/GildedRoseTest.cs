8b3953ebfcde7798180b8ecb010d50614b6ef418
Steve Turley <steve@turleylabs.com>
2020-07-06 14:14:27 -0500

    Update GildedRoseTest.cs

--- GildedRoseRefactoring/GildedRoseTest.cs
+++ GildedRoseRefactoring/GildedRoseTest.cs
@@ -1,9 +1,21 @@
 using NUnit.Framework;
 using System;
 
+using ApprovalTests;
+using ApprovalTests.Core;
+using ApprovalTests.Reporters;
+using ApprovalUtilities;
+
 [TestFixture]
+[UseReporter(typeof(QuietReporter))]
 public class GildedRoseTest
 {
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        Approvals.RegisterDefaultNamerCreation(() => new CyberDojoNamer());
+    }
+    
     [Test]
     public void ApproveGildedRose()
     {

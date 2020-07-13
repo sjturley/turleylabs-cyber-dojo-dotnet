1b3a2260cfef1c270fcb73f23fbb9e296c3b6cbd
Steve Turley <steve@turleylabs.com>
2020-07-13 09:33:02 -0500

    Update GildedRoseTest.cs

--- GildedRoseRefactoring/GildedRoseTest.cs
+++ GildedRoseRefactoring/GildedRoseTest.cs
@@ -20,4 +20,22 @@ public class GildedRoseTest
     public void ApproveGildedRose()
     {
     }
+    
+    void approveGildedRose() {
+        String[] names = {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
+        Integer[] sellins = { -1, 0, 12, 6, 2 };
+        Integer[] qualities = { 0, 1, 48, 49, 50 };
+        CombinationApprovals.verifyAllCombinations(
+                this::runGildedRose,
+                names,
+                sellins,
+                qualities);
+    }
+
+    private Item runGildedRose(String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateItems();
+        return gildedRose.items[0];
+    }
 }

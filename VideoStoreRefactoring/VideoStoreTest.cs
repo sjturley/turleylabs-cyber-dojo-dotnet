eca0a53ba83dfcf9ed60a77894755eedc541bd22
Steve Turley <steve@turleylabs.com>
2020-07-13 10:17:41 -0500

    Update VideoStoreTest.cs

--- VideoStoreRefactoring/VideoStoreTest.cs
+++ VideoStoreRefactoring/VideoStoreTest.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 
 using ApprovalTests;
 using ApprovalTests.Core;
+using ApprovalTests.Combinations;
 using ApprovalTests.Reporters;
 using ApprovalUtilities;
 
@@ -20,6 +21,16 @@ public class VideoStoreTest
     [Test]
     public void RENAME_ME()
     {
-        Approvals.Verify("Me");
+        CombinationApprovals.VerifyAllCombinations(CreateCustomerStatement
+                , new int[] {Movie.NEW_RELEASE, Movie.REGULAR, Movie.CHILDRENS}
+                , new int[] {1, 2, 3, 4});
+    }
+    
+    private string CreateCustomerStatement(int priceCode, int daysRented) {
+        Customer customer = new Customer("Steve");
+        Movie movie = new Movie("Star Wars", Movie.REGULAR);
+        customer.AddRental (new Rental(movie, daysRented));
+        movie.SetPriceCode(priceCode);
+        return customer.statement();
     }
 }
